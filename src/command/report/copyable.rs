085e559f80c722560233ec6daf4145814fc31f71
campbellcole <10430178+campbellcole@users.noreply.github.com>
2023-06-01 19:22:39 -0700

    finish copyable report, bump version to 0.8.0

--- src/command/report/copyable.rs
+++ src/command/report/copyable.rs
@@ -13,20 +13,28 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 use std::{
+    fs::File,
     io::{Read, Write},
     process::{Command, Stdio},
 };
 
-use polars::prelude::{DataFrame, LazyFrame};
+use polars::prelude::LazyFrame;
+use snailquote::escape;
 
 use crate::{
     prelude::*,
-    table::{color::Color, settings::TableSettings, style::TableStyle, DataFrameDisplay},
+    table::{settings::TableSettings, style::TableStyle, DataFrameDisplay},
 };
 
-use super::{daily, weekly, ReportSettings, ReportType, COL_DURATION};
+use super::{daily, weekly, ReportSettings, ReportType};
 
 const MARKDOWN_TEMPLATE: &str = include_str!("../../../web/template.md");
+const HTML_TEMPLATE: &str = include_str!("../../../web/template.html");
+
+const REPORT_DATE_PLACEHOLDER: &str = "%%REPORT_DATE%%";
+const REPORT_TABLE_PLACEHOLDER: &str = "%%REPORT_TABLE%%";
+const TOTAL_HOURS_PLACEHOLDER: &str = "%%TOTAL_HOURS%%";
+const REPORT_HTML_PLACEHOLDER: &str = "%%REPORT_HTML%%";
 
 pub fn generate_copyable_report(lf: LazyFrame, settings: &ReportSettings) -> Result<()> {
     let mut table = String::new();
@@ -55,11 +63,11 @@ pub fn generate_copyable_report(lf: LazyFrame, settings: &ReportSettings) -> Res
     let mut template = String::from(MARKDOWN_TEMPLATE);
 
     template = template.replace(
-        "%%REPORT_DATE%%",
+        REPORT_DATE_PLACEHOLDER,
         &Local::now().format("%Y-%m-%d").to_string(),
     );
 
-    template = template.replace("%%REPORT_TABLE%%", &table);
+    template = template.replace(REPORT_TABLE_PLACEHOLDER, &table);
 
     // this table retains original data types so we can use it to calculate the total hours
     let df = lf.collect()?;
@@ -69,7 +77,7 @@ pub fn generate_copyable_report(lf: LazyFrame, settings: &ReportSettings) -> Res
     let total_hours = BiDuration::new(total_hours);
     let total_hours_str = total_hours.to_friendly_hours_string();
 
-    template = template.replace("%%TOTAL_HOURS%%", &total_hours_str);
+    template = template.replace(TOTAL_HOURS_PLACEHOLDER, &total_hours_str);
 
     let mut pandoc = Command::new("pandoc");
     pandoc.stdin(Stdio::piped()).stdout(Stdio::piped());
@@ -87,6 +95,33 @@ pub fn generate_copyable_report(lf: LazyFrame, settings: &ReportSettings) -> Res
 
     stdout.read_to_string(&mut html)?;
     drop(stdout);
+
+    html = escape(&html).to_string();
+
+    let full_html = HTML_TEMPLATE.replace(REPORT_HTML_PLACEHOLDER, &html);
+
+    let temp_dir = temp_dir::TempDir::new()?;
+
+    let temp_file_path = temp_dir.path().join("report.html");
+
+    let mut temp_file = File::create(&temp_file_path)?;
+
+    write!(temp_file, "{}", full_html)?;
+    temp_file.flush()?;
+
+    println!("Opening report in browser...");
+    println!("Follow instructions on the page that opens.");
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    let mut chromium = Command::new("chromium");
+
+    chromium.stdout(Stdio::null()).stderr(Stdio::null());
+
+    chromium.arg(&temp_file_path);
+
+    chromium.spawn()?.wait()?;
+
+    println!("Report closed.");
 
     Ok(())
 }
